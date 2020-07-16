22bcb729be3da03c32bb598a5a9c05814cc7feb8
Sandip Pal <sandippal@yahoo.com>
2020-07-16 11:40:15 -0700

    Add code for local training
    Not flattened as the other model

--- python/local_mnist.py
+++ python/local_mnist.py
@@ -10,16 +10,28 @@ from tensorflow.keras.datasets import mnist
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data(path=current_dir+'/../db/mnist.npz')
 
 
-(ds_train, ds_test), ds_info = tfds.load(
-    current_dir+'/../db/mnist.npz',
-    split=['train', 'test'],
-    shuffle_files=True,
-    as_supervised=True,
-    with_info=True,
-)
-
 print('MNIST Dataset Shape:')
 print('X_train: ' + str(X_train.shape))
 print('Y_train: ' + str(Y_train.shape))
 print('X_test:  '  + str(X_test.shape))
 print('Y_test:  '  + str(Y_test.shape))
+
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+  tf.keras.layers.Dense(128,activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    metrics=['accuracy'],
+)
+
+model.fit(
+    x=X_train,
+    y=Y_train,
+    epochs=8,
+    validation_data=(X_test, Y_test),
+)
+
