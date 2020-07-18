8f76930526371c0c0488caa15ea82198a4b7c6a6
Sandip Pal <sandippal@yahoo.com>
2020-07-17 22:17:05 -0700

    Do a clasification from the test data
    Just printing one of the random image

--- python/local_mnist.py
+++ python/local_mnist.py
@@ -1,20 +1,29 @@
 #Author : Sandip Pal
 #File : local_mnist
 #Purpose : Train a model with MNIST training sample
-#
+#          Classify our own image
+#          Next classify an entire page with numericals
 
 
 # Get the working directory path
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 import os
 current_dir = os.getcwd()
+
+
+
 
 # Import mnist data stored in the following path: current directory -> mnist.npz
 
 from tensorflow.keras.datasets import mnist
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data(path=current_dir+'/../db/mnist.npz')
 
+
+image_index = 7777 # You may select anything up to 60,000
+print(Y_train[image_index]) # The label is 8
+plt.imshow(X_train[image_index], cmap='Greys')
 
 print('MNIST Dataset Shape:')
 print('X_train: ' + str(X_train.shape))
@@ -40,4 +49,9 @@ model.fit(
     epochs=8,
     validation_data=(X_test, Y_test),
 )
+
+image_index = 4444
+plt.imshow(X_test[image_index].reshape(28, 28),cmap='Greys')
+pred = model.predict(X_test[image_index].reshape(1, 28, 28, 1))
+print(pred.argmax())
 
