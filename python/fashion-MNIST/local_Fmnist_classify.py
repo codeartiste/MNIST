2e8eb32e3ea5203d7d8a5ad2b79d14299f0a5960
Sandip Pal <sandippal@yahoo.com>
2020-09-22 18:06:07 -0700

    FMNSIT working training and clasifier

--- python/fashion-MNIST/local_Fmnist_classify.py
+++ python/fashion-MNIST/local_Fmnist_classify.py
@@ -20,19 +20,159 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
+'''
 imgmp = mpimg.imread('myhandwritennums/number.png')
 print(imgmp.shape)
 imgmp = 1 - imgmp  # Done to match black to white and vice versa with Keras db
 imgmp = imgmp.reshape(1, 28, 28, 1)
+'''
+def normalize_img(x):
+    ''' Normalizes images: uint8 -> float32 '''
+    x = x.astype('float32')
+    x = x / 255.0
+    return x
 
 
-rec_model = tensorflow.keras.models.load_model('mnistmodel')
+# load  dataset
+def load_dataset_and_normalize2():
+    #current_dir = os.getcwd()
+    fashion_mnist = keras.datasets.fashion_mnist
+    # load dataset
+    (trainX, trainY), (testX, testY) = fashion_mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    trainX = normalize_img(trainX)
+    testX = normalize_img(testX)
+    return trainX, trainY, testX, testY
+    
+    
 
 
-pred = rec_model.predict(imgmp)
-print(pred.argmax())
-plt.imshow(imgmp.reshape(28, 28) , cmap='Greys')
+
+model = tensorflow.keras.models.load_model('fmnistmodel')
+
+
+X_train, Y_train, X_test, Y_test = load_dataset_and_normalize2()
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+print(X_train.shape)
+print(len(Y_train))
+print(Y_train)
+print(X_test.shape)
+print(len(Y_test))
+plt.figure()
+plt.imshow(X_train[0].reshape(28,28))
+plt.colorbar()
+plt.grid(False)
 plt.show()
+
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(X_train[i].reshape(28,28), cmap=plt.cm.binary)
+    plt.xlabel(class_names[Y_train[i]])
+plt.show()
+
+
+
+predictions = model.predict(X_test)
+print(predictions[0])
+print(np.argmax(predictions[0]))
+print(Y_test[0])
+
+def plot_image(i, predictions_array, true_label, img):
+  true_label, img = true_label[i], img[i].reshape(28,28)
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  true_label = true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+i = 1
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], Y_test, X_test)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  Y_test)
+plt.show()
+
+i = 22
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], Y_test, X_test)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  Y_test)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+offset = 50
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i+offset, predictions[i+offset ], Y_test, X_test)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i+offset, predictions[i+offset], Y_test)
+plt.tight_layout()
+plt.show()
+
+# Grab an image from the test dataset.
+img = X_test[11]
+
+print(img.shape)
+
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+
+predictions_single = model.predict(img)
+
+print('-------Prediction------')
+print(predictions_single)
+
+plot_value_array(11, predictions_single[0], Y_test)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+print(np.argmax(predictions_single[0]))
+
 
 
 
--- python/fashion-MNIST/local_Fmnist_train.py
+++ python/fashion-MNIST/local_Fmnist_train.py
@@ -2,7 +2,7 @@
 #File : local_Fmnist_training
 #Purpose : Train a model with Fashion MNIST training sample
 #          Train the model with 60,000 images
-#          Next classify an entire page with numericals
+#          Next classify an entire page with photos of apparels
 
 # TensorFlow and tf.keras
 import tensorflow.compat.v2 as tf
@@ -10,8 +10,11 @@ import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import tensorflow.keras.models
 import matplotlib.image as mpimg
-import numpy as np
 import os
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
@@ -20,9 +23,25 @@ from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
 
+def normalize_img(x):
+    ''' Normalizes images: uint8 -> float32 '''
+    x = x.astype('float32')
+    x = x / 255.0
+    return x
+
+
 # load  dataset
-def load_dataset_and_normalize():
-    current_dir = os.getcwd()
+def load_dataset_and_normalize1():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    return train_images, train_labels, test_images, test_labels
+
+
+# load  dataset
+def load_dataset_and_normalize2():
+    #current_dir = os.getcwd()
     fashion_mnist = keras.datasets.fashion_mnist
     # load dataset
     (trainX, trainY), (testX, testY) = fashion_mnist.load_data()
@@ -42,9 +61,7 @@ def define_model1():
     model.add(Dense(128, activation=tf.nn.relu))
     model.add(Dropout(0.2))
     model.add(Dense(10))
-    model.compile(optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
     return model
 
 
@@ -73,138 +90,28 @@ def define_model3():
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 print(tf.__version__)
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+X_train, Y_train, X_test, Y_test = load_dataset_and_normalize2()
 
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-train_images.shape
-len(train_labels)
-train_labels
-test_images.shape
-len(test_labels)
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-train_images = train_images / 255.0
-test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
-
+'''
 model = define_model1();
 model.fit(train_images, train_labels, epochs=15)
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-
 print('\nTest accuracy:', test_acc)
 
-probability_model = tf.keras.Sequential([model,
-tf.keras.layers.Softmax()])
-predictions = probability_model.predict(test_images)
-predictions[0]
-np.argmax(predictions[0])
-test_labels[0]
+'''
 
-def plot_image(i, predictions_array, true_label, img):
-  true_label, img = true_label[i], img[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-
-  plt.imshow(img, cmap=plt.cm.binary)
-
-  predicted_label = np.argmax(predictions_array)
-  if predicted_label == true_label:
-    color = 'blue'
-  else:
-    color = 'red'
-
-  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
-
-def plot_value_array(i, predictions_array, true_label):
-  true_label = true_label[i]
-  plt.grid(False)
-  plt.xticks(range(10))
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+model = define_model2();
+model.fit(X_train, Y_train, epochs=12)
+test_loss, test_acc = model.evaluate(X_test,  Y_test, verbose=2)
+print('\nTest accuracy:', test_acc)
 
 
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-# Plot the first X test images, their predicted labels, and the true labels.
-# Color correct predictions in blue and incorrect predictions in red.
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions[i], test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions[i], test_labels)
-plt.tight_layout()
-plt.show()
-
-# Grab an image from the test dataset.
-img = test_images[1]
-
-print(img.shape)
-
-
-# Add the image to a batch where it's the only member.
-img = (np.expand_dims(img,0))
-
-print(img.shape)
-
-
-predictions_single = probability_model.predict(img)
-
-print('Prediction')
-print(predictions_single)
-
-plot_value_array(1, predictions_single[0], test_labels)
-_ = plt.xticks(range(10), class_names, rotation=45)
-plt.show()
-
-print(np.argmax(predictions_single[0]))
 
 model.save('fmnistmodel')
 
